ae485b0c757d136a76b72051e192dda63aea32cf
Roberto Robson <betorobson@gmail.com>
2025-07-10 15:25:04 -0300

    jest
    init select virtual scroll
    
    git ignore node_modules

---
+++ projects/angular-boost-kit/src/lib/material-select-virtual-scroll/material-select-virtual-scroll.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Input, input, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport, ScrollingModule } from '@angular/cdk/scrolling';
+import { ScrollingModule as ExperimentalScrollingModule } from '@angular/cdk-experimental/scrolling';
+import { MatFormField, MatLabel, MatOption, MatSelect, MatSelectTrigger } from '@angular/material/select';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'lib-material-select-virtual-scroll',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ScrollingModule,
+    ExperimentalScrollingModule,
+    MatFormField,
+    MatLabel,
+    MatSelect,
+    MatSelectTrigger,
+    MatOption
+  ],
+  templateUrl: './material-select-virtual-scroll.html',
+  styleUrl: './material-select-virtual-scroll.css'
+})
+export class MaterialSelectVirtualScroll {
+
+  @Input({required: true}) formControl = new FormControl<number | null>(null);
+
+	@ViewChild(CdkVirtualScrollViewport, { static: false })
+		cdkVirtualScrollViewPort: CdkVirtualScrollViewport | undefined;
+
+  protected options = Array.from({length: 1000}).map((value, index) => ({
+    id: index + 1,
+    desc: `Item ${index + 1}`
+  }))
+
+  protected openedChange(){
+    this.openedChangeCDKVirtualScroll();
+  }
+
+  private openedChangeCDKVirtualScroll(){
+    if(this.cdkVirtualScrollViewPort){
+      this.cdkVirtualScrollViewPort.checkViewportSize();
+    }
+  }
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,24 @@
 import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { FancyButton, TesteClass } from 'angular-boost-kit';
+import { FancyButton, MaterialSelectVirtualScroll, TesteClass } from 'angular-boost-kit';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FancyButton],
+  imports: [RouterOutlet, FancyButton, MaterialSelectVirtualScroll],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+
   protected title = 'angular-boot-kit';
+
   x = new TesteClass();
+
+  selectVirtualScrollFormControl = new FormControl<number | null>(null);
+
   constructor(){
     console.log(this.x.getNumber());
   }
+
 }
