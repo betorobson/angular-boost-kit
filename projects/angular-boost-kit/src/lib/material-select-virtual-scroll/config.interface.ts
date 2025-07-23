b588fcce3659f86f43dea1d4dbdbd31461bc60cb
Roberto Robson <betorobson@gmail.com>
2025-07-23 16:53:49 -0300

    cities mock

--- projects/angular-boost-kit/src/lib/material-select-virtual-scroll/config.interface.ts
+++ projects/angular-boost-kit/src/lib/material-select-virtual-scroll/config.interface.ts
@@ -2,7 +2,7 @@ import { FormControl } from "@angular/forms";
 import { Observable } from "rxjs";
 
 export interface MaterialSelectVirtualScrollConfig<T = any> {
-  formControl: FormControl<number>;
+  formControl: FormControl;
   load: () => Observable<Array<T>>;
   populateBasedOnFormControls?: FormControl[];
   optionItemId: keyof T;
