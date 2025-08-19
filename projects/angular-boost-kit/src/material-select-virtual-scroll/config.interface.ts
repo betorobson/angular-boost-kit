c73d972f8456bea90f70a83187b6a3752619fb3f
Roberto Robson <betorobson@gmail.com>
2025-08-19 10:23:02 -0300

    rename

---
+++ projects/angular-boost-kit/src/material-select-virtual-scroll/config.interface.ts
@@ -0,0 +1,22 @@
+import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
+
+export interface MaterialSelectVirtualScrollConfig<
+  T = any
+> {
+  templateDataOptionMeta?: OptionMetaData<T>,
+  templateDataMultipleOptionMeta?: OptionMetaData<T>[],
+  formControl: FormControl;
+  multiple?: boolean;
+  hideResetOption?: boolean;
+  replaceArrowByResetButton?: boolean;
+  load: () => Observable<Array<T>>;
+  populateBasedOnFormControls?: FormControl[];
+  optionItemDescription: keyof T;
+  compositeId?: Array<Extract<keyof T, string>>;
+}
+
+export interface OptionMetaData<T = any> {
+  id: Partial<T>;
+  data: T;
+}
