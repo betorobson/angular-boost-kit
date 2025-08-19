ac83caadb4979b60d242e713b82cafc92debdb20
Roberto Robson <betorobson@gmail.com>
2025-08-19 10:22:56 -0300

    rename

--- projects/angular-boost-kit/src/lib/cdk/template-type/template-type.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface BoostKitTemplateType<T = any>{
-	templateData: T
-}
--- projects/angular-boost-kit/src/public-api.ts
+++ projects/angular-boost-kit/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of angular-boost-kit
  */
 
-export * from './lib/material-select-virtual-scroll/material-select-virtual-scroll';
-export * from './lib/material-select-virtual-scroll/config.interface';
-export * from './lib/cdk/public-api';
+export * from './material-select-virtual-scroll/material-select-virtual-scroll';
+export * from './material-select-virtual-scroll/config.interface';
+export * from './cdk/public-api';
